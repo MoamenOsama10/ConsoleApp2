e0aae41d4212efb7c6b83538480b3c60e84b9b55
MoamenOsama10 <momenosama985@gmail.com>
2025-07-09 10:26:38 +0300

    firstComent

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine( "Welcome To GITHUB");
+            int X=10;
+            Console.WriteLine(X);
         }
     }
 }
